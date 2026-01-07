7d1be78f274b35a0a6270758727c56c3fa10bf3a
Your Name <your_email@example.com>
2026-01-07 10:03:51 +0300

    updated main

--- controllers/order.js
+++ controllers/order.js
@@ -1,11 +1,12 @@
 const Order = require('../models/order');
 const express = require('express');
 const router= express.Router()
+const verifyToken = require('../middleware/verify-token');
 
-router.post('/', async(req,res)=>{
+router.post('/',verifyToken, async(req,res)=>{
     try{
         console.log(req.user)
-        req.body.user = req.user
+        req.body.user = req.user._id
         const createdOrder = await Order.create(req.body);
         res.status(201).json(createdOrder)
     }catch(error){
@@ -14,10 +15,10 @@ router.post('/', async(req,res)=>{
     }
 })
 
-router.get('/', async(req,res)=>{
+router.get('/',verifyToken, async(req,res)=>{
     try{
 
-        const foundOrders = await Order.find();
+        const foundOrders = await Order.find({user: req.user._id});
         res.status(200).json(foundOrders);
         
     }catch(error){
@@ -26,7 +27,7 @@ router.get('/', async(req,res)=>{
     }
 })
 
-router.get('/:orderId', async(req,res)=>{
+router.get('/:orderId',verifyToken, async(req,res)=>{
         try{
     
             console.log(req.params.orderId)
@@ -45,7 +46,7 @@ router.get('/:orderId', async(req,res)=>{
         }}
     })
 
-router.delete('/:orderId', async(req,res)=>{
+router.delete('/:orderId',verifyToken, async(req,res)=>{
         try{
  
         const deleteOrder = await Order.findByIdAndDelete(req.params.orderId);
@@ -63,9 +64,9 @@ router.delete('/:orderId', async(req,res)=>{
         }}
     })
 
-router.put('/:orderId', async(req,res)=>{
+router.put('/:orderId',verifyToken, async(req,res)=>{
     try{
-        const updatedOrder = await Order.findByIdAndUpdate(req.params.orderId)
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.orderId , req.body, {new: true} )
         if (!updatedOrder){
             res.status(404);
             throw new Error('Order not found!');
