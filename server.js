d9bccca21d37969f0803f51d6374e4bffffcd735
hiba-GA <hiba.formal@gmail.com>
2026-01-08 10:06:54 +0300

    update

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const express = require('express');
-
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
